532460e4f46690025d6d2e2fce4861d627d8de88
Thanoon12k <engthanoon1@gmail.com>
2025-08-07 14:00:34 +0300

    prefer the first loading indicator

--- lib/common_widgets/loading_indicator.dart
+++ lib/common_widgets/loading_indicator.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 class GeneratorLoadingIndicator extends StatefulWidget {
   const GeneratorLoadingIndicator({super.key});
@@ -12,23 +11,13 @@ class GeneratorLoadingIndicator extends StatefulWidget {
 class _GeneratorLoadingIndicatorState extends State<GeneratorLoadingIndicator>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
-  static const int dotCount = 10;
-  static const double orbitRadius = 35;
-
-  final List<Color> dotColors = [
-    Colors.yellowAccent,
-    Colors.lightBlueAccent,
-    Colors.amberAccent,
-    Colors.blueAccent,
-    Colors.orangeAccent,
-  ];
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 3),
+      duration: const Duration(seconds: 2),
     )..repeat();
   }
 
@@ -38,85 +27,82 @@ class _GeneratorLoadingIndicatorState extends State<GeneratorLoadingIndicator>
     super.dispose();
   }
 
-  Widget _buildOrbitingDot(int index) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) {
-        final angle =
-            (_controller.value * 2 * pi) + (index * 2 * pi / dotCount);
-        final x = cos(angle) * orbitRadius;
-        final y = sin(angle) * orbitRadius;
-
-        final wave = sin((_controller.value * 2 * pi) + (index * pi / 5));
-        final scale = 0.7 + 0.3 * wave;
-        final opacity = 0.5 + 0.5 * wave;
-
-        final color = dotColors[index % dotColors.length];
-
-        return Transform.translate(
-          offset: Offset(x, y),
-          child: Transform.scale(
-            scale: scale,
-            child: Opacity(
-              opacity: opacity,
-              child: Container(
-                width: 9,
-                height: 9,
-                decoration: BoxDecoration(
-                  color: color,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: color.withOpacity(0.6),
-                      blurRadius: 4,
-                      spreadRadius: 1,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildCenterCore() {
-    return Container(
-      width: 32,
-      height: 32,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        gradient: const RadialGradient(
-          colors: [Colors.amber, Colors.blueAccent],
-          center: Alignment.center,
-          radius: 0.8,
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.blueAccent.withOpacity(0.6),
-            blurRadius: 20,
-            spreadRadius: 6,
-          ),
-        ],
-      ),
-      child: const Center(
-        child: Icon(Icons.flash_on, size: 16, color: Colors.white),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
         width: 120,
-        height: 120,
+        height: 60,
         child: Stack(
           alignment: Alignment.center,
           children: [
-            ...List.generate(dotCount, _buildOrbitingDot),
-            _buildCenterCore(),
+            // Left moving block
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                final moveAnimation = Tween<double>(begin: -60, end: 0).animate(
+                  CurvedAnimation(
+                    parent: _controller,
+                    curve: const Interval(0.0, 0.5, curve: Curves.easeInOut),
+                  ),
+                );
+                return Transform.translate(
+                  offset: Offset(moveAnimation.value, 0),
+                  child: Container(
+                    width: 40,
+                    height: 20,
+                    color: Colors.blueGrey.shade300,
+                  ),
+                );
+              },
+            ),
+            // Right moving block
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                final moveAnimation = Tween<double>(begin: 60, end: 0).animate(
+                  CurvedAnimation(
+                    parent: _controller,
+                    curve: const Interval(0.0, 0.5, curve: Curves.easeInOut),
+                  ),
+                );
+                return Transform.translate(
+                  offset: Offset(moveAnimation.value, 0),
+                  child: Container(
+                    width: 40,
+                    height: 20,
+                    color: Colors.blueGrey.shade300,
+                  ),
+                );
+              },
+            ),
+            // Center spinning part (the "generator")
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                final spinAnimation = Tween<double>(
+                  begin: 0,
+                  end: 2 * 3.14,
+                ).animate(
+                  CurvedAnimation(
+                    parent: _controller,
+                    curve: const Interval(0.5, 1.0, curve: Curves.linear),
+                  ),
+                );
+                return Transform.rotate(
+                  angle: spinAnimation.value,
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
